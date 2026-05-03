83bb85a4c2a6fd631006cb0fce5040cd6162696e
Souraouiali <souraouiali@gmail.com>
2026-05-03 14:35:20 +0100

    fix: allow cdn.sanity.io images in Next.js config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'cdn.sanity.io' },
     ],
   },
 }
